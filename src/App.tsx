48d06625e92f84ddc90688f5247a953faefd0437
Tiago Oliveira <tiagolivesan@outlook.com>
2021-03-18 13:21:09 -0300

    dt-money Ignite

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import styled from 'styled-components'
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  font-size: 24px;
-  color: #8457e6;
-`
+
+
 
 export function App() {
   return (
     <div className="App">
-      <Title>
-        Hello Wordl!
-      </Title>
+        <h1>Hello World!</h1>
+        <GlobalStyle />
     </div>
   );
 }
