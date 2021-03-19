4163c2f979f5fd216fafbf47b7f1b460089d2664
Tiago Oliveira <tiagolivesan@outlook.com>
2021-03-19 19:10:17 -0300

    dt-money Ignite

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+
+  const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] = useState(false);
+
+    function handleOpenNewTransactionsModalOpen(){
+        setIsNewTransactionsModalOpen(true);
+    }
+    function handleCloseNewTransactionsModalOpen(){
+        setIsNewTransactionsModalOpen(false);
+    }
+
   return (
     <>
-        <Header />
+        <Header onOpenNewTransactionModal = {handleOpenNewTransactionsModalOpen}/>
         <Dashboard />
         <GlobalStyle />
+        <NewTransactionModal 
+          isOpen={isNewTransactionsModalOpen}
+          onRequestClose={handleCloseNewTransactionsModalOpen}
+        />
     </>
   );
 }
