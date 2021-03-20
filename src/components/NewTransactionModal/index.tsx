1147545fb254b85c9da980a70ae50a59b4cc0389
Tiago Oliveira <tiagolivesan@outlook.com>
2021-03-19 21:02:12 -0300

    dt-money Ignite

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,12 @@
 import Modal from 'react-modal';
 
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from "../../assets/close.svg"
+
+import {Container, TransactionTypeContainer } from './styles'
+
+
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
@@ -10,8 +17,55 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <Modal
           isOpen={isOpen}
           onRequestClose={onRequestClose}
+          overlayClassName="react-modal-overlay"
+          className="react-modal-content"
         >
-          <h2>cadastrar</h2>
+        
+        <button type="button" 
+            onClick={onRequestClose} 
+            className="react-modal-close"
+        >
+            <img src={closeImg} alt="Fechar Modal"/>
+        </button>
+
+        <Container>
+            <h2>Cadastrar transação</h2>
+            
+            <input 
+            placeholder="Título"
+            />
+
+            <input 
+            type="Number"
+            placeholder="Valor"
+            />
+
+            <TransactionTypeContainer>
+                <button
+                    type="button"
+                >
+                    <img src={incomeImg} alt="Entrada" />
+                    <span>Entrada</span>
+                </button>
+
+                <button
+                    type="button"
+                >
+                    <img src={outcomeImg} alt="Saída" />
+                    <span>Saída</span>
+                </button>
+            </TransactionTypeContainer>
+
+            <input 
+            placeholder="Categoria"
+            />
+
+            <button type="submit">
+                Cadastrar
+            </button>
+
+        </Container>
+
         </Modal>
     )
 }
