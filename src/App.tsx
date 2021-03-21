00a0943a76f365986d0520295c9e4ac86c8bdf6c
Tiago Oliveira <tiagolivesan@outlook.com>
2021-03-21 17:02:29 -0300

    dt-money Ignite

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './TransactionsContext';
 
 export function App() {
 
   const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] = useState(false);
 
-    function handleOpenNewTransactionsModalOpen(){
+  function handleOpenNewTransactionsModalOpen(){
         setIsNewTransactionsModalOpen(true);
-    }
-    function handleCloseNewTransactionsModalOpen(){
+  }
+  function handleCloseNewTransactionsModalOpen(){
         setIsNewTransactionsModalOpen(false);
-    }
+  }
 
   return (
-    <>
+    <TransactionsProvider> 
         <Header onOpenNewTransactionModal = {handleOpenNewTransactionsModalOpen}/>
+
         <Dashboard />
-        <GlobalStyle />
+
         <NewTransactionModal 
           isOpen={isNewTransactionsModalOpen}
           onRequestClose={handleCloseNewTransactionsModalOpen}
         />
-    </>
+
+        <GlobalStyle />
+    </TransactionsProvider>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,15 @@
+import React, {useContext} from 'react';
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from "./styles";
 
 export function Summary() {
+    const transactions = useContext(TransactionsContext);
+
     return(
         <Container>
             <div>
